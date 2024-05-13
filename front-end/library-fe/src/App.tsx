bb6cb95f586f29c55cd3bc28ee5e5ab3e936caab
diyaniliev96 <diyan.iliev96@gmail.com>
2024-05-13 15:29:33 +0300

    Add navbar

--- front-end/library-fe/src/App.tsx
+++ front-end/library-fe/src/App.tsx
@@ -1,11 +1,10 @@
 
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() { 
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+  <Navbar/>
   );
 }
 
